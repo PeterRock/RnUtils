f9fd3be92370b36b40d0227bdbd5659a22179540
Peter Shi <usstszy@163.com>
2016-07-01 16:15:57 +0800

    初始化仓库并添加了千位分隔符方法

---
+++ README.md
@@ -0,0 +1,19 @@
+# React Native Utils
+
+收集编写常用的React Native中会用到的方法或者小组件
+目前有的方法：
+
+  - 添加千位分隔符
+
+### Version
+0.0.1
+
+License
+----
+
+MIT
+
+
+**Free Software, Enjoy it!**
+
+
---
+++ rnutils.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  NativeModules,
+} from 'react-native';
+
+const RCTUtilsManager = NativeModules.Utils;
+
+const RnUtils = {
+  /*
+   添加千分位分隔符
+  */
+  milliFormat = (() => {
+    const DIGIT_PATTERN = /(^|\s)\d+(?=\.?\d*($|\s))/g;
+    const MILLI_PATTERN = /(?=(?!\b)(\d{3})+$)/g;
+
+    return (input) => input && input.toString()
+      .replace(DIGIT_PATTERN, (m) => m.replace(MILLI_PATTERN, ','));
+  })(),
+  isOnCall: function (callback) {
+    RCTUtilsManager.isOnCall(function (response) {
+      if (callback) {
+        callback(response === "yes")
+      }
+    })
+  }
+};
+
+module.exports = RnUtils;
